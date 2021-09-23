1afedf1d1eb1fbb5be53396497d17f73a7927f9c
suriya-it19 <54205777+suriya-it19@users.noreply.github.com>
2021-09-23 21:16:37 +0530

    initial package creation

---
+++ pip-package/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='easydistributions',
+      version='0.1',
+      description='Easy to use distributions, such as Gaussian, Binomial added with utility features',
+      packages=['distributions'],
+      zip_safe=False)
