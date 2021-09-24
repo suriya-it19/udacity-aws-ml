3f7a282633212690e1664354eb0d57cde376e350
suriya-it19 <54205777+suriya-it19@users.noreply.github.com>
2021-09-24 11:49:39 +0530

    version update

--- pip-package/setup.py
+++ pip-package/setup.py
@@ -1,7 +1,9 @@
 from setuptools import setup
 
-setup(name='easydistributions',
-      version='0.1',
-      description='Easy to use distributions, such as Gaussian, Binomial added with utility features',
-      packages=['distributions'],
-      zip_safe=False)
+setup(
+    name="easydistributions",
+    version="0.1",
+    description="Easy to use distributions, such as Gaussian, Binomial added with utility features",
+    packages=["distributions"],
+    zip_safe=False,
+)
